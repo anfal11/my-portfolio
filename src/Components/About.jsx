0577621ced27c4cb273ff1ea2334915529876b04
anfal11 <anfal15-14303@diu.edu.bd>
2023-12-12 18:24:52 +0600

    resume/cv downlaod implemented

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,19 +2,19 @@ import pic from "../../public/dsdf.JPG"
 const About = () => {
     return (
         <div className="mt-64 mb-40">
-            <h1 className="text-4xl underline text-center font-bold mb-4">
-        About Me
-      </h1>
+            
         <div className="hero">
-          <div className="hero-content flex flex-col lg:flex-row items-center justify-center">
+          <div className="hero-content flex flex-col lg:flex-row items-center justify-center gap-4">
             <img
               src={pic}
               alt="Profile"
               className="rounded-full shadow-2xl mb-6 lg:mb-0 lg:mr-6"
             />
-            <div className="text-center ">
-
-              <p className="py-4 text-neutral-800">
+            <div>
+            <h1 className="text-4xl underline text-center font-bold mb-4">
+        About Me
+      </h1>
+              <p className="py-4 ">
   Greetings! I&apos;m Anfal, a soon-to-graduate computer science student at Daffodil International University. Specializing in the MERN Stack, my journey is marked by a keen passion for innovation. Eagerly seeking opportunities, I am poised to apply my skills to cutting-edge projects, craft engaging user experiences, and play a part in shaping the future of the web. As a fresh graduate, I bring a dynamic and enthusiastic approach to contribute effectively to your team.
 </p>
 
--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,23 +1,51 @@
-import { IoMdDownload } from "react-icons/io";
-import pic from '../../public/pic2.jpg'
-import "./Banner.css"
+import { IoMdDownload } from 'react-icons/io';
+import pic from '../../public/pic2.jpg';
+import './Banner.css';
+
+const pdf = '../../public/latestCV.pdf';
 
 const Banner = () => {
-    return (
-        <div className="max-w-[1520px] mx-auto">
-            <div className="flex flex-col lg:flex-row justify-between items-center mt-20 mx-20 gap-10">
-                <div className="flex flex-col items-center justify-center text-center">
-                    <h1 className="text-5xl font-bold mb-4">Hello, I am Anfal</h1>
-                    <h2 className="text-2xl font-semibold mb-4">Junior MERN Stack Developer</h2> 
-                    <p className="text-lg mb-6">I am passionate about creating responsive and user-friendly web experiences. Skilled in HTML, CSS, JavaScript, React, ExpressJs and MongoDB.</p>
-                    <button className="flex px-2 py-2 rounded-lg items-center gap-2 bg-black text-white hover:bg-gray-700">
-                        <IoMdDownload /> Download CV
-                    </button>               
-                </div>
-                <img src={pic} className="one w-96 lg:w-[430px] xl:w-[550px]" alt="Anfal Image" />
-            </div>
+  const downloadPDF = () => {
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = pdf;
+
+    // Wait for the iframe to load the PDF content
+    iframe.onload = () => {
+      const doc = iframe.contentDocument || iframe.contentWindow.document;
+      const downloadLink = doc.createElement('a');
+      downloadLink.href = pdf;
+      downloadLink.download = 'Anfal_CV.pdf';
+      doc.body.appendChild(downloadLink);
+      downloadLink.click();
+      doc.body.removeChild(downloadLink);
+      iframe.remove();
+    };
+
+    document.body.appendChild(iframe);
+  };
+
+  return (
+    <div className="max-w-[1520px] mx-auto">
+      <div className="flex flex-col lg:flex-row justify-between items-center mt-20 mx-20 gap-10">
+        <div className="flex flex-col items-center justify-center text-center">
+          <h1 className="text-5xl font-bold mb-4">Hello, I am Anfal</h1>
+          <h2 className="text-2xl font-semibold mb-4">Junior MERN Stack Developer</h2>
+          <p className="text-lg mb-6">
+            I am passionate about creating responsive and user-friendly web experiences. Skilled in HTML, CSS, JavaScript,
+            React, ExpressJs, and MongoDB.
+          </p>
+          <button
+            onClick={downloadPDF}
+            className="flex px-2 py-2 rounded-lg items-center gap-2 bg-black text-white hover:bg-gray-700"
+          >
+            <IoMdDownload /> Download CV
+          </button>
         </div>
-    );
+        <img src={pic} className="one w-96 lg:w-[430px] xl:w-[550px]" alt="Anfal Image" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
