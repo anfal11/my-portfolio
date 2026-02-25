bd08629a3371d8e605eaa4a887335f0d3bae5450
anfal11 <anfal15-14303@diu.edu.bd>
2026-02-25 06:41:09 +0600

    resume updated in banner section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
       const doc = iframe.contentDocument || iframe.contentWindow.document;
       const downloadLink = doc.createElement("a");
       downloadLink.href = pdf;
-      downloadLink.download = "cv_of_anfal.pdf";
+      downloadLink.download = "resume_of_anfal.pdf";
       doc.body.appendChild(downloadLink);
       downloadLink.click();
       doc.body.removeChild(downloadLink);
@@ -45,14 +45,14 @@ const Banner = () => {
     }}>
     </motion.div> */}
       <div className="flex flex-col lg:flex-row justify-between items-center mt-20 mx-20 gap-10">
-        <div 
-        data-aos="fade-right"
-        data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
+        <div
+          data-aos="fade-right"
+          data-aos-offset="200"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="false"
         >
           <div className="flex flex-col items-center justify-center text-center">
             <h1 className="text-5xl font-bold mb-4">Hello, I am Anfal</h1>
@@ -70,23 +70,27 @@ const Banner = () => {
             <p className="text-lg mb-6">
               {/* I am passionate about creating responsive and user-friendly web applications and  Skilled in HTML, CSS, JavaScript, React, ExpressJs,
               and MongoDB. */}
-              DevOps Engineer: CI/CD, Docker, Kubernetes, IaC using Terraform, Ansible, AWS, Azure, GCP proficiency for seamless deployment and management.
+              DevOps Engineer: CI/CD, Docker, Kubernetes, IaC using Terraform,
+              Ansible, AWS, Azure, GCP proficiency for seamless deployment and
+              management.
             </p>
             <button
               onClick={downloadPDF}
               className="flex px-2 py-2 rounded-lg items-center gap-2 bg-black text-white hover:bg-gray-700"
             >
-              <IoMdDownload /> Download CV
+              <IoMdDownload /> Download Resume
             </button>
           </div>
         </div>
-        <div data-aos="fade-left"
-        data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false">
+        <div
+          data-aos="fade-left"
+          data-aos-offset="200"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="false"
+        >
           <img
             src={pic}
             className="one w-96 lg:w-[430px] xl:w-[550px]"
