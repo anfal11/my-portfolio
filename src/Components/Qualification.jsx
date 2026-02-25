d8a97aa14239b286d116043447e83e7c77ed2307
anfal11 <anfal15-14303@diu.edu.bd>
2026-02-25 06:26:39 +0600

    qualification section updated (jp added)

--- src/Components/Qualification.jsx
+++ src/Components/Qualification.jsx
@@ -30,6 +30,32 @@ const Qualification = () => {
                 />
               </svg>
             </div>
+
+            <div className="timeline-end mb-10">
+              <time className="font-mono italic">2026</time>
+              <div className="text-lg font-black">Research Student</div>
+              University Of Fukui, Japan <br />
+    
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
             <div className="timeline-start md:text-end mb-10">
               <time className="font-mono italic">2024</time>
               <div className="text-lg font-black">
@@ -38,7 +64,7 @@ const Qualification = () => {
               Daffodil International University, Ashulia, Savar, Dhaka <br />
               CGPA: 3.28
             </div>
-           
+
             <hr />
           </li>
           <li>
@@ -57,7 +83,7 @@ const Qualification = () => {
                 />
               </svg>
             </div>
-           
+
             <div className="timeline-end mb-10">
               <time className="font-mono italic">2019</time>
               <div className="text-lg font-black">HSC</div>
